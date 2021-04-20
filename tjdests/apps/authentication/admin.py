e3e8a0b3706c4e2c478828ec45b170e4c91d1649
Ethan Nguyen <etnguyen03@hotmail.com>
2021-04-19 21:49:16 -0400

    feat: add search to django admin

--- tjdests/apps/authentication/admin.py
+++ tjdests/apps/authentication/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ["username", "first_name", "last_name"]
+
+
+admin.site.register(User, UserAdmin)
--- tjdests/apps/destinations/admin.py
+++ tjdests/apps/destinations/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import College, Decision, TestScore
 
-admin.site.register(College)
+
+class CollegeAdmin(admin.ModelAdmin):
+    search_fields = ["ceeb_code", "name", "location"]
+
+
+admin.site.register(College, CollegeAdmin)
 admin.site.register(TestScore)
 admin.site.register(Decision)
