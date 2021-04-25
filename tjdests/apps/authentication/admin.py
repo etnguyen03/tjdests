5bd755ebe0b5695a3c9e030d41ce3c5e1fea2081
Ethan Nguyen <etnguyen03@hotmail.com>
2021-04-25 16:33:53 -0400

    feat(authentication): add filter by show data in admin

--- tjdests/apps/authentication/admin.py
+++ tjdests/apps/authentication/admin.py
@@ -6,7 +6,7 @@ from .models import User
 class UserAdmin(admin.ModelAdmin):
     search_fields = ["username", "first_name", "last_name"]
     list_display = ["username", "last_name", "first_name", "last_modified"]
-    list_filter = ["is_senior", "accepted_terms", "is_student"]
+    list_filter = ["is_senior", "is_student", "accepted_terms", "publish_data"]
 
     fieldsets = (
         (
