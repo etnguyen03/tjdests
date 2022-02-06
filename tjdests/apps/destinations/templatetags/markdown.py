aa2df1935e1117b86da9f95d8afe6a8a5f850014
Shreyas Mayya <2022smayya@tjhsst.edu>
2022-02-05 23:13:01 -0500

    feat(destinations): use mistune for Markdown parsing

--- tjdests/apps/destinations/templatetags/markdown.py
+++ tjdests/apps/destinations/templatetags/markdown.py
@@ -1,15 +1,28 @@
-from markdown import markdown
+import mistune
+from pygments import highlight
+from pygments.formatters import html
+from pygments.lexers import get_lexer_by_name
 
 from django import template
 
-from .strikethrough import StrikethroughExtension
-
 register = template.Library()
+
+
+class HighlightRenderer(mistune.HTMLRenderer):
+    def block_code(self, code, info=None):
+        if info:
+            lexer = get_lexer_by_name(info, stripall=True)
+            formatter = html.HtmlFormatter()
+            return highlight(code, lexer, formatter)
+        return "<pre><code>" + mistune.escape(code) + "</code></pre>"
 
 
 @register.filter(name="markdown")
 def convert_markdown(text: str):
     """Convert text to markdown HTML."""
-    return markdown(
-        text, extensions=["extra", "codehilite", "smarty", StrikethroughExtension()]
+    markdown = mistune.create_markdown(
+        renderer=HighlightRenderer(),
+        plugins=["footnotes", "strikethrough", "table"],
+        escape=False,
     )
+    return markdown(text)
