de27f33c8fa67189ccfa8e9458d40d6cf6506bf9
Krishnan Shankar <krishnans2006@gmail.com>
2024-01-30 14:57:51 -0500

    feat: add prod defaults to secret.sample.py

--- tjdests/settings/secret.sample.py
+++ tjdests/settings/secret.sample.py
@@ -18,9 +18,34 @@ SECRET_KEY = "supersecret"
 SOCIAL_AUTH_ION_KEY = "ionkey"
 SOCIAL_AUTH_ION_SECRET = "ionsecret"
 
+# Database
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ["DIRECTOR_DATABASE_NAME"],
+#         "USER": os.environ["DIRECTOR_DATABASE_USERNAME"],
+#         "PASSWORD": os.environ["DIRECTOR_DATABASE_PASSWORD"],
+#         "HOST": os.environ["DIRECTOR_DATABASE_HOST"],
+#         "PORT": os.environ["DIRECTOR_DATABASE_PORT"],
+#     },
+# }
+
+# Axes
+# AXES_META_PRECEDENCE_ORDER = [
+#     'HTTP_X_REAL_IP',
+# ]
+# AXES_NEVER_LOCKOUT_WHITELIST = True
+# AXES_IP_WHITELIST = ["151.188.227.237"]
+
 # Message blast - treated as HTML safe text
 # type is str
 GLOBAL_MESSAGE = None
+# GLOBAL_MESSAGE = "<b>WARNING</b>: This is not ready for production usage! Any and all data may be deleted at any time without warning!"
 
 # Login lock: if True, restrict login to superusers only
 LOGIN_LOCKED = False
+
+TIME_ZONE = "America/New_York"
+
+# To prevent Git issues
+MAINTAINER = "First Last (20XXusername)"
